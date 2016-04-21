0f51655c6e115aa1d53167dd665d139a9f8b6163
Seth Kerr <sethmsk88@yahoo.com>
2016-04-21 11:25:35 -0400

    Add files

---
+++ content/links.php
@@ -0,0 +1,13 @@
+<?php
+	include_once $_SERVER['DOCUMENT_ROOT'] . '/bootstrap/apps/shared/db_connect.php';
+
+?>
+
+<div id="links-container" class="container">
+	<div class="row">
+		<a href="#">Sample Link 1</a>
+	</div>
+	<div class="row">
+		<a href="#">Sample Link 2</a>
+	</div>
+</div>
---
+++ index.php
@@ -0,0 +1,91 @@
+<?php
+	define("APP_NAME", "Senior Leadership Meeting Links");
+	define("APP_PATH", "http://" . $_SERVER['HTTP_HOST'] . "./bootstrap/apps/login_system/");
+    define("APP_HOMEPAGE", "links");
+
+	include_once $_SERVER['DOCUMENT_ROOT'] . '/bootstrap/apps/shared/dbInfo.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?= APP_NAME ?></title>
+
+    <!-- Linked stylesheets -->
+    <link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
+    <link href="../css/navbar-custom1.css" rel="stylesheet">
+    <link href="../css/master.css" rel="stylesheet">
+    <link href="./css/main.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <!-- Google Analytics Tracking -->
+    <?php include_once($_SERVER['DOCUMENT_ROOT'] . "\bootstrap\apps\shared\analyticstracking.php") ?>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Included Scripts -->
+    <script src="./scripts/main.js"></script>
+
+    <?php
+    	// Include FAMU logo header
+        include "../templates/header_3.php";
+    ?>
+
+	<!-- Nav Bar -->
+	<nav
+        id="pageNavBar"
+        class="navbar navbar-default navbar-custom1 navbar-static-top"
+        role="navigation"
+        >
+        <div class="container">
+            <div class="navbar-header">
+                <button
+                    type="button"
+                    class="navbar-toggle"
+                    data-toggle="collapse"
+                    data-target="#navbarCollapse"
+                    >
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="?page=<?= APP_HOMEPAGE ?>"><?= APP_NAME ?></a>
+            </div>
+        </div>
+    </nav>
+
+
+    <?php
+        // If a page variable exists, include the page
+    	if (isset($_GET["page"])){
+    		$filePath = './content/' . $_GET["page"] . '.php';
+    	}
+    	else{
+    		$filePath = './content/' . APP_HOMEPAGE . '.php';
+    	}
+
+    	if (file_exists($filePath)){
+			include $filePath;
+		}
+		else{
+			echo '<h2>404 Error</h2>Page does not exist';
+		}
+    ?>
+
+  </body>
+</html>
